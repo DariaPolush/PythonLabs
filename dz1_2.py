6073653704aa5ae8c46ea6efa146276f79e68b19
Daria Polushina <144628414+DariaPolush@users.noreply.github.com>
2023-09-15 11:45:20 +0300

    Update dz1_2.py

--- dz1_2.py
+++ dz1_2.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(-9,9,0.001)
-plt.plot(x, ((x**3 - 4)/((x-1)**3)))
-plt.axis([-9,9,-5,5])
+
+x = np.arange(-9, 9, 0.001)
+y = (x ** 3 - 4) / ((x - 1) ** 3)
+
+plt.plot(x, y)
+plt.axis([-9, 9, -5,5])
 plt.grid()
 plt.show()
 
