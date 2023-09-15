5e5b81a091d6b2d3cde34364a894de97287d46a7
Daria Polushina <144628414+DariaPolush@users.noreply.github.com>
2023-09-15 11:59:34 +0300

    Update dz1_4.py

--- dz1_4.py
+++ dz1_4.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-6, 6)
-plt.plot(x, (np.arcsin(2*np.abs(x)/(1+x**2))))
+x = np.arange(-6, 6, 0.001)
+
+plt.plot(x, (np.arcsin(2 * np.abs(x) / (1 + x ** 2))))
 plt.grid()
 plt.show()
