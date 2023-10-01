abc21a6e925aed9019cf639cbc19b70473d1a86c
Daria Polushina <144628414+DariaPolush@users.noreply.github.com>
2023-10-01 23:56:26 +0300

    Create dz4_r2.py

---
+++ dz4_r2.py
@@ -0,0 +1,10 @@
+a = float(input())
+n = int(input())
+def power(a, n):
+    if n == 0:
+        return 1
+    elif n > 0:
+        return a * power(a, n - 1)
+    else:
+        return 1 / a * power(a, n + 1)
+print(power(a, n))
